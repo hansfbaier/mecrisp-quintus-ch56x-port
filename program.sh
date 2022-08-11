aa9f3ed1412bc396177370d9c8d20dd710db2114
Hans Baier <hansfbaier@gmail.com>
2022-08-11 09:05:02 +0700

    program.sh: make it quite and resume execution

--- program.sh
+++ program.sh
@@ -3,6 +3,8 @@
 /opt/wch-openocd/bin/openocd -f /opt/wch-openocd/bin/wch-riscv.cfg &
 sleep 1
 nc localhost 4444 <<EOF
-program mecrisp-quintus-ch56x.hex 0 verify reset
+program mecrisp-quintus-ch56x.hex 0 verify
+wlink_reset_resume
+exit
 EOF
 pkill openocd
